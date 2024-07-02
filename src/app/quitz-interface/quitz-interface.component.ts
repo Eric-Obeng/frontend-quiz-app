e88639f69e61caf1bbaadcc8c8c79a7003f8ecfe
Paa Kwesi Ephraim <peekaylink@gmail.com>
2024-07-02 10:23:59 +0000

    starting

---
+++ src/app/quitz-interface/quitz-interface.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quitz-interface',
+  standalone: true,
+  imports: [],
+  templateUrl: './quitz-interface.component.html',
+  styleUrl: './quitz-interface.component.css'
+})
+export class QuitzInterfaceComponent {
+
+}
