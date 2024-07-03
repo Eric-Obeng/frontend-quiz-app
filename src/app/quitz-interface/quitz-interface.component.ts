7aad3ba3cfb3c47e12dc68d60afe3b2bb5f6a47a
Jhay-Great <alemonai.jenef@gmail.com>
2024-07-03 17:08:01 +0000

    completed quiz ui

--- src/app/quitz-interface/quitz-interface.component.ts
+++ src/app/quitz-interface/quitz-interface.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { QuizQuestionsComponent } from '../quiz-questions/quiz-questions.component';
 import { LandingPageComponent } from '../landing-page/landing-page.component';
+import {CompletedQuizComponent} from '../completed-quiz/completed-quiz.component';
 
 @Component({
   selector: 'app-quitz-interface',
   standalone: true,
-  imports: [QuizQuestionsComponent, LandingPageComponent],
+  imports: [QuizQuestionsComponent, LandingPageComponent, CompletedQuizComponent],
   templateUrl: './quitz-interface.component.html',
   styleUrl: './quitz-interface.component.css',
 })
