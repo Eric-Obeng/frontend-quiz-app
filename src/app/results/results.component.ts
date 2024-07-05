dd260da1a9a02ccdc7c86c8b2d8564a6dad434e5
Jhay-Great <alemonai.jenef@gmail.com>
2024-07-05 10:52:38 +0000

    added router link to the imports

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './results.component.html',
   styleUrl: './results.component.css'
 })
