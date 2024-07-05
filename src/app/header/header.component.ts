78fbbbc7eefc7983d44e3fe3b9ad1314f01690d8
Eric-Obeng <obengeric.3958@gmail.com>
2024-07-05 08:48:54 +0000

    header component new update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,12 @@
-import { Component, inject } from '@angular/core';
-import { ThemeService } from '../services/theme.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
+  styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  toggleActive = false;
-  themeService = inject(ThemeService);
 
-  handleClick() {
-    // toggle the button
-    this.toggleActive = !this.toggleActive;
-
-    // toggele the theme
-    this.themeService.toggleTheme();
-  }
 }
